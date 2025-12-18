d643790e1fc1c43355988f0b4528a767c3d96b7d
Nitay4 <nitay@rotbart.co.il>
2025-12-18 19:28:21 +0200

    herman for nitay

--- src/main/java/frc/constants/field/TowerSide.java
+++ src/main/java/frc/constants/field/TowerSide.java
@@ -5,10 +5,10 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public enum TowerSide {
 
-	CLOSE_TOWER(new Pose2d(Field.Tower.getX() - 0.4, Field.Tower.getY(), Rotation2d.k180deg)),
-	FAR_TOWER(new Pose2d(Field.Tower.getX() + 0.4, Field.Tower.getY(), Rotation2d.kZero)),
-	RIGHT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() - 0.4, Rotation2d.kCCW_90deg.times(-1))),
-	LEFT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() + 0.4, Rotation2d.kCCW_90deg));
+	CLOSE_TOWER(new Pose2d(Field.Tower.getX() - 0.27, Field.Tower.getY(), Rotation2d.k180deg)),
+	FAR_TOWER(new Pose2d(Field.Tower.getX() + 0.27, Field.Tower.getY(), Rotation2d.kZero)),
+	RIGHT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() - 0.27, Rotation2d.kCCW_90deg.times(-1))),
+	LEFT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() + 0.27, Rotation2d.kCCW_90deg));
 
 	private final Pose2d towerSide;
 
