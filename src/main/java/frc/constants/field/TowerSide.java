9ea94adca92cf24975ae0fe8e5429b5fe354b999
Nitay4 <nitay@rotbart.co.il>
2025-12-21 19:58:21 +0200

    nitay - it woooooorks

--- src/main/java/frc/constants/field/TowerSide.java
+++ src/main/java/frc/constants/field/TowerSide.java
@@ -5,10 +5,10 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public enum TowerSide {
 
-	CLOSE_TOWER(new Pose2d(Field.Tower.getX() - 0.27, Field.Tower.getY(), Rotation2d.k180deg)),
-	FAR_TOWER(new Pose2d(Field.Tower.getX() + 0.27, Field.Tower.getY(), Rotation2d.kZero)),
-	RIGHT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() - 0.27, Rotation2d.kCCW_90deg.times(-1))),
-	LEFT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() + 0.27, Rotation2d.kCCW_90deg));
+	CLOSE_TOWER(new Pose2d(Field.Tower.getX() - Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Field.Tower.getY(), Rotation2d.k180deg)),
+	FAR_TOWER(new Pose2d(Field.Tower.getX() + Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Field.Tower.getY(), Rotation2d.kZero)),
+	RIGHT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() - Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Rotation2d.kCCW_90deg.times(-1))),
+	LEFT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() + Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Rotation2d.kCCW_90deg));
 
 	private final Pose2d towerSide;
 
