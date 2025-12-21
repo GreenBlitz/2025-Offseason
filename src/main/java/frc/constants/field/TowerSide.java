35d9f4fed4ee0273366e342a2ddc9f1b992e7e3f
Nitay4 <nitay@rotbart.co.il>
2025-12-21 19:58:25 +0200

    nitay - it woooooorks

--- src/main/java/frc/constants/field/TowerSide.java
+++ src/main/java/frc/constants/field/TowerSide.java
@@ -7,7 +7,9 @@ public enum TowerSide {
 
 	CLOSE_TOWER(new Pose2d(Field.Tower.getX() - Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Field.Tower.getY(), Rotation2d.k180deg)),
 	FAR_TOWER(new Pose2d(Field.Tower.getX() + Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Field.Tower.getY(), Rotation2d.kZero)),
-	RIGHT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() - Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Rotation2d.kCCW_90deg.times(-1))),
+	RIGHT_TOWER(
+		new Pose2d(Field.Tower.getX(), Field.Tower.getY() - Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Rotation2d.kCCW_90deg.times(-1))
+	),
 	LEFT_TOWER(new Pose2d(Field.Tower.getX(), Field.Tower.getY() + Field.TOWER_SIDES_DISTANCE_FROM_MIDDLE_METERS, Rotation2d.kCCW_90deg));
 
 	private final Pose2d towerSide;
