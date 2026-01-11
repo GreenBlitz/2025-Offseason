16e79d6e78a630464bdafea643a96f1c68632596
tomer-hershman <lili.hershman@gmail.com>
2026-01-11 19:25:05 +0200

    tomer- YHrm CR

--- src/main/java/frc/robot/statemachine/ShooterCalculations.java
+++ src/main/java/frc/robot/statemachine/ShooterCalculations.java
@@ -2,13 +2,14 @@ package frc.robot.statemachine;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.SimulationManager;
+import frc.robot.subsystems.constants.turret.TurretConstants;
 
 public class ShooterCalculations {
 
 	public static Pose2d getTurretPose(Pose2d robotPose) {
 		return new Pose2d(
-			robotPose.getX() + robotPose.getRotation().getCos() * SimulationManager.TURRET_DISTANCE_FROM_ROBOT_ON_X_AXIS,
-			robotPose.getY() + robotPose.getRotation().getSin() * SimulationManager.TURRET_DISTANCE_FROM_ROBOT_ON_X_AXIS,
+			robotPose.getX() + robotPose.getRotation().getCos() * TurretConstants.TURRET_DISTANCE_FROM_ROBOT_ON_X_AXIS,
+			robotPose.getY() + robotPose.getRotation().getSin() * TurretConstants.TURRET_DISTANCE_FROM_ROBOT_ON_X_AXIS,
 			robotPose.getRotation()
 		);
 	}
